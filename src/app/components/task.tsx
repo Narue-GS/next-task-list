0eccd8b9bfa7948feef8255173b7fc2ca4c5ac3b
Naruê Gonçalves Santos <naruegit@gmail.com>
2023-08-17 18:59:54 -0300

    fix: fix a bug in task's state conditional style

--- src/app/components/task.tsx
+++ src/app/components/task.tsx
@@ -7,12 +7,12 @@ interface TaskProps {
 }
 
 export default function Task(props:TaskProps) {
-  return(
+    return(
     <li className="flex px-2 place-items-center">
       <div className="flex justify-between place-items-center w-full">
         <span>{props.data.title}</span>
         <span
-          className={`cursor-pointer text-${props.data.isComplete? 'green-400' : 'red-400'}`}
+          className={`cursor-pointer ${props.data.isComplete? 'text-green-400' : 'text-red-400'}`}
           onClick={() => props.updateState(props.data.id)}
         >{props.data.isComplete ? ":)" : ":|"}</span>
       </div>
